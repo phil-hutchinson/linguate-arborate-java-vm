e9e8bae15d379115bc273deacdaa624ecfd71e7a
phil-hutchinson <phil-hutchinson@users.noreply.github.com>
2019-02-23 15:44:48 -0800

    List Object - simplify logic for insertPosition() and removePosition()
    Advances https://github.com/phil-hutchinson/linguate-arborate-java-vm/issues/22

--- src/org/linguate/arborate/vm/ArborateList.java
+++ src/org/linguate/arborate/vm/ArborateList.java
@@ -43,56 +43,42 @@ public class ArborateList extends ArborateObject {
     }
     
     public ArborateList insertPosition(long pos, ArborateObject val) {
-        if (pos < 0) {
+        if (pos < 0 || pos > items.length) {
             return this;
-        } else if (pos == 0) {
-            // insert at start
-            ArborateObject newItems[] = new ArborateObject[items.length + 1];
-            newItems[0] = val;
-            System.arraycopy(items, 0, newItems, 1, items.length);
-            return new ArborateList(newItems);
-        } else if (pos < items.length) {
-            // insert in middle
-            ArborateObject newItems[] = new ArborateObject[items.length + 1];
+        } 
+        
+        ArborateObject newItems[] = new ArborateObject[items.length + 1];
+
+        if (pos > 0) {
             System.arraycopy(items, 0, newItems, 0, (int)pos);
-            newItems[(int)pos] = val;
-            System.arraycopy(items, (int)pos, newItems, (int)pos + 1, items.length - (int)pos);
-            return new ArborateList(newItems);
-        } else if (pos == items.length ) {
-            // insert at end
-            ArborateObject newItems[] = new ArborateObject[items.length + 1];
-            System.arraycopy(items, 0, newItems, 0, items.length);
-            newItems[(int)pos] = val;
-            return new ArborateList(newItems);
-        } else {
-            return this;
         }
+        
+        newItems[(int)pos] = val;
+
+        if (pos < items.length) {
+            System.arraycopy(items, (int)pos, newItems, (int)pos + 1, items.length - (int)pos);
+        }
+        
+        return new ArborateList(newItems);
     }
 
     public ArborateList removePosition(long pos) {
-        if (pos < 0) {
+        if (pos < 0 || pos >= items.length) {
             return this;
-        } else if (pos >= items.length) {
-            return this;
-        } else if (pos == 0) {
-            // remove from start
-            ArborateObject newItems[] = new ArborateObject[items.length - 1];
-            if (items.length > 1) {
-                System.arraycopy(items, 1, newItems, 0, items.length);
-            }
-            return new ArborateList(newItems);
-        } else if (pos < items.length - 1) {
-            // remove from middle
-            ArborateObject newItems[] = new ArborateObject[items.length - 1];
-            System.arraycopy(items, 0, newItems, 0, (int)pos);
+        } 
+
+        ArborateObject newItems[] = new ArborateObject[items.length - 1];
+        
+        
+        if (pos > 0) {
+            System.arraycopy(items, 0, newItems, 0, items.length);
+        } 
+        
+        if (pos < items.length - 1) {
             System.arraycopy(items, (int)pos + 1, newItems, (int)pos, items.length - 1 - (int)pos);
-            return new ArborateList(newItems);
-        } else {
-            // remove from end
-            ArborateObject newItems[] = new ArborateObject[items.length - 1];
-            System.arraycopy(items, 0, newItems, 0, items.length - 1);
-            return new ArborateList(newItems);
         }
+
+        return new ArborateList(newItems);
     }
     
     public int getSize() {
